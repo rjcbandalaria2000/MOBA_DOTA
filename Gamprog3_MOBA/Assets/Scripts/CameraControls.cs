8dd79f65ca109541ec7db17640454f3d07f38e84
Jameson Chiu <andrechiu69@gmail.com>
2021-09-01 22:14:37 +0800

    Add Camera Controls & Map Layout

--- Gamprog3_MOBA/Assets/Scripts/CameraControls.cs
+++ Gamprog3_MOBA/Assets/Scripts/CameraControls.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
-    [SerializeField] private float panSpeed = 50.0f;
+    [SerializeField] private float panSpeed = 10.0f;
+    [SerializeField] private float panBorder = 10.0f;
+    public Vector2 panLimit;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,28 @@ public class CameraControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+
+        if (Input.mousePosition.y >= Screen.height - panBorder)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <= panBorder)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x >= Screen.width - panBorder)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x <= panBorder)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+        transform.position = pos;
     }
 }
