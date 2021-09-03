b9f15a468ad88ea1430aedc662ed1f37b9d2e71e
Jameson Chiu <andrechiu69@gmail.com>
2021-09-04 00:49:51 +0800

    Merge branch 'Chris-Repository' of https://gitlab.com/jamesonandre.chiu/gamprog3-moba into Chris-Repository
    # Conflicts:
    #	Gamprog3_MOBA/Logs/AssetImportWorker0-prev.log
    #	Gamprog3_MOBA/Logs/shadercompiler-UnityShaderCompiler.exe1.log
    #	Gamprog3_MOBA/Logs/shadercompiler-UnityShaderCompiler.exe2.log

---
+++ Gamprog3_MOBA/Assets/Assets/MinimapScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    [SerializeField]
+    Camera      mainCamera;
+    [SerializeField]
+    Camera      minimapCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        minimapCamera = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (Input.GetMouseButtonDown(0))
+        {
+            //RaycastHit mapClick;
+            ////Ray ray = minimapCamera.ScreenPointToRay(Input.mousePosition);
+            //          //Camera.current.ScreenPointToRay(Input.mousePosition);
+
+            //if (Physics.Raycast(ray, out mapClick, 100.0f))
+            //{
+            //    if (mapClick.transform != null)
+            //    {
+            //        Debug.Log("Mouse Position: " + mapClick.transform.position);
+            //    }
+            //}
+
+            Vector3 cameraPosition = minimapCamera.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("Where I clicked " + cameraPosition);
+            mainCamera.transform.position = new Vector3(cameraPosition.x, mainCamera.transform.position.y, cameraPosition.z);
+        }
+
+    }
+}
