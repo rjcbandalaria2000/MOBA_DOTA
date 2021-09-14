1ebf36ba4f0d32e78e69e201c5a03de140126088
Ronald Jon Christopher Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2021-09-14 21:20:30 +0800

    Created Unit Scripts

---
+++ Gamprog3_MOBA/Assets/Scripts/Unit Scripts/UnitStats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public float baseDamage;
+    public int strength;
+    public int intelligence;
+    public int agility; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
