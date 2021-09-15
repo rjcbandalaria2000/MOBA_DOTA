ab8007a73e2013cfe6c62cf599094b3b54d650be
Ronald Jon Christopher Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2021-09-15 20:30:08 +0800

    Created Test Scene for the Selecting

--- Gamprog3_MOBA/Assets/Scripts/Test Scripts/SelectControlPrototype.cs
+++ Gamprog3_MOBA/Assets/Scripts/Test Scripts/SelectControlPrototype.cs
@@ -1,32 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class PlayerControls : MonoBehaviour
+public class SelectControlPrototype : MonoBehaviour
 {
-    //public GameObject player;
-
-    public NavMeshAgent player;
-
     // Start is called before the first frame update
     void Start()
     {
-        player = this.GetComponent<NavMeshAgent>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray.origin, ray.direction, out hit))
             {
-                player.SetDestination(hit.point);
+                Debug.Log("Object Clicked: " + hit.transform.name);
             }
         }
-        
     }
 }
