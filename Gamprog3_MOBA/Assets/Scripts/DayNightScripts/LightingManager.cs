c57d4a833d7ae4b90677c7daeb518ccabc7dd171
Ronald Jon Christopher Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2021-10-29 21:02:40 +0800

    Temporary fix to the day and night

--- Gamprog3_MOBA/Assets/Scripts/DayNightScripts/LightingManager.cs
+++ Gamprog3_MOBA/Assets/Scripts/DayNightScripts/LightingManager.cs
@@ -11,14 +11,23 @@ public class LightingManager : MonoBehaviour
     Light directionalLight;
     [SerializeField]
     LightingPreset lightPreset;
+    [SerializeField]
+    float dayTimeChange;
+    [SerializeField]
+    float transitionTimer = 0;
+    [SerializeField]
+    float dayTransitionTime = 5f;
+    bool isDay = true;
 
-    [SerializeField, Range(0,600)]
+    [SerializeField, Range(0,30)]
     public float dayTimer; // LIGHTING -> DAY/NIGHT CYCLE
+
+    private Coroutine dayTransitionRoutine;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isDay = true;
     }
 
     // Update is called once per frame
@@ -31,8 +40,15 @@ public class LightingManager : MonoBehaviour
         if (Application.isPlaying)
         {
             dayTimer += Time.deltaTime;
-            dayTimer %= 600; // Clamped between 0-600
-            ChangeLighting(dayTimer / 600);
+            //dayTimer %= dayTimeChange;
+            if(dayTimer >= dayTimeChange)
+            {
+               dayTransitionRoutine = StartCoroutine(ChangeDayNight());
+                dayTimer = 0;
+            }
+            // Clamped between 0-600
+            
+            //ChangeLighting(dayTimer / dayTimeChange);
             //Separate time for tracking the dayTime and do coroutine when daytime goes to 5mins 
             //at least 5 sec transitiontime from day to night 
         }
@@ -43,7 +59,45 @@ public class LightingManager : MonoBehaviour
     {
         RenderSettings.ambientLight = lightPreset.ambientColor.Evaluate(timeOfDay);
         directionalLight.color = lightPreset.directionalColor.Evaluate(timeOfDay);
-        directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timeOfDay * 360f) - 90f, 170, 0));
+        if (isDay) {
+            //If Day, switch to night
+            directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timeOfDay * 360f) - 45f, 0, 0));
+        }
+        else if (!isDay)
+        {
+            // If night, switch to day
+            directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timeOfDay * 360f) + 45f, 0, 0));
+        }
+        
 
+    }
+    IEnumerator ChangeDayNight()
+    {
+        yield return new WaitForSeconds(0f);
+        Debug.Log("DayNight Routine");
+        if (transitionTimer < dayTransitionTime)
+        {
+            Debug.Log("Night Time");
+            while (transitionTimer <= dayTransitionTime)
+            {
+                transitionTimer += Time.deltaTime;
+                ChangeLighting(transitionTimer / dayTransitionTime);
+            }
+            isDay = false;
+        }
+        else if (transitionTimer >= dayTransitionTime)
+        {
+            Debug.Log("Day Time");
+            while (transitionTimer > 0)
+            {
+                transitionTimer -= Time.deltaTime;
+                if (transitionTimer < 0)
+                {
+                    transitionTimer = 0f;
+                }
+                ChangeLighting(transitionTimer / dayTransitionTime);
+            }
+            isDay = true;
+        }
     }
 }
