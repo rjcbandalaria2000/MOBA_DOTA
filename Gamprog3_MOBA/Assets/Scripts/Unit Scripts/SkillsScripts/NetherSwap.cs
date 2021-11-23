c6fe2f25c7569a3acc2c95ebb712e52728387c86
Ronald Jon Christopher Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2021-11-23 23:13:04 +0800

    Added interrupt to Nether Swap

--- Gamprog3_MOBA/Assets/Scripts/Unit Scripts/SkillsScripts/NetherSwap.cs
+++ Gamprog3_MOBA/Assets/Scripts/Unit Scripts/SkillsScripts/NetherSwap.cs
@@ -19,6 +19,11 @@ public class NetherSwap : Skill
         attacker.transform.position = targetPos;
         target.transform.position = currentPos;
 
+        Animator targetAnimator = target.GetComponent<Animator>();
+        
+        InterruptActions(targetAnimator);
+        
+
         attacker.GetComponent<NavMeshAgent>().SetDestination(targetPos);
     }
 
@@ -27,5 +32,18 @@ public class NetherSwap : Skill
         base.ActivateSkill(target, attacker);
         isCoolDown = true;
         coolDownRoutine = StartCoroutine(SkillCoolDown(skillCooldown[skillLevel - 1], skillIndex));
+    }
+    void InterruptActions(Animator targetAnimator)
+    {
+        Debug.Log("Interrupt");
+        AI_Script unitAI = targetAnimator.gameObject.GetComponent<AI_Script>();
+        if(unitAI)
+        {
+            unitAI.targets.Clear();
+        }
+        targetAnimator.SetBool("IsMoving", false);
+        targetAnimator.SetBool("IsAttacking", false);
+        targetAnimator.gameObject.GetComponent<Unit>().target = null;
+        targetAnimator.SetBool("hasTarget", false);
     }
 }
